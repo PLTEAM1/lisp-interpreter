be588ecfd9f3e8cfa5876216bd2a7880c53a174e
SimHongSub <ghdtjq2038@gmail.com>
2020-11-16 04:06:59 +0900

    [basic] modify syntax.cpp

--- src/Syntax.cpp
+++ src/Syntax.cpp
@@ -1,8 +1,11 @@
 #include "../header/Syntax.h"
+#include "../header/Basic.h"
 
-void Syntax::check_Syntax(){
+string Syntax::check_Syntax(vector< pair<int, string> > token){
 
     /* Syntax 판별 기능 구현 */
+
+    return "1";
 }
 
 void Syntax::arithmetic_Operation(){
@@ -13,51 +16,7 @@ void Syntax::arithmetic_Operation(){
 void Syntax::basic_Function(vector< pair<int, string> > token){
 
     /* 기본 함수 기능 구현 */
-    string function_Name = token[0].second;
-
-    if(function_Name == "SETQ"){
-        /* SETQ Function ex) SETQ X 3); */
-
-    }else if(function_Name == "LIST"){
-        /* LIST Function */
-
-    }else if(function_Name == "CAR"){
-        /* CAR Function */
-
-    }else if(function_Name == "CDR"){
-        /* CDR Function */
-
-    }else if(function_Name == "CADDR"){
-        /* CADDR Function */
-
-    }else if(function_Name == "NTH"){
-        /* NTH Function */
-
-    }else if(function_Name == "CONS"){
-        /* CONS Function */
-
-    }else if(function_Name == "REVERSE"){
-        /* REVERSE Function */
-
-    }else if(function_Name == "APPEND"){
-        /* APPEND Function */
-
-    }else if(function_Name == "LENGTH"){
-        /* LENGTH Function */
-
-    }else if(function_Name == "MEMBER"){
-        /* MEMBER Function */
-
-    }else if(function_Name == "ASSOC"){
-        /* ASSOC Function */
-
-    }else if(function_Name == "REMOVE"){
-        /* REMOVE Function */
-
-    }else{
-        /* SUBST Function */
-    }
-    
+        
 }
 
 void Syntax::predicate_Finction(){
@@ -70,13 +29,71 @@ void Syntax::conditional(){
     /* 조건문 구현 */
 }
 
-void Syntax::analyze(vector< pair<int, string> > token){
+string Syntax::analyze(vector< pair<int, string> > token, vector< pair<string, string> > *variables){
 
-    //token = t;
+    Basic basic;
 
-    /*while(!token.empty()){
-        check_Syntax();
-    }*/
+    string function_Name = token[0].second;
 
-    basic_Function(token);
+    if(function_Name == "SETQ"){
+        /* SETQ Function ex) SETQ X (1 2 3)); */
+        return basic.setq(token, variables);
+
+    }else if(function_Name == "LIST"){
+        /* LIST Function */
+        return basic.list(token, variables);
+
+    }else if(function_Name == "CAR"){
+        /* CAR Function */
+        return basic.car(token);
+
+    }else if(function_Name == "CDR"){
+        /* CDR Function */
+        return basic.cdr(token);
+
+    }else if(function_Name == "CADDR"){
+        /* CADDR Function */
+        return basic.caddr(token);
+
+    }else if(function_Name == "NTH"){
+        /* NTH Function */
+        return basic.nth(token);
+
+    }else if(function_Name == "CONS"){
+        /* CONS Function */
+        return basic.cons(token);
+
+    }else if(function_Name == "REVERSE"){
+        /* REVERSE Function */
+        return basic.reverse(token);
+
+    }else if(function_Name == "APPEND"){
+        /* APPEND Function */
+        return basic.append(token);
+
+    }else if(function_Name == "LENGTH"){
+        /* LENGTH Function */
+        return basic.length(token);
+
+    }else if(function_Name == "MEMBER"){
+        /* MEMBER Function */
+        return basic.member(token);
+
+    }else if(function_Name == "ASSOC"){
+        /* ASSOC Function */
+        return basic.assoc(token);
+
+    }else if(function_Name == "REMOVE"){
+        /* REMOVE Function */
+        return basic.remove(token);
+
+    }else if(function_Name == "SUBST"){
+        /* SUBST Function */
+        return basic.subst(token);
+
+    }else{
+        return "1";
+    }
+
+    return "0";
 }
