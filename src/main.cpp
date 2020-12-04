51e37e9f0bee3dc77eff132c8e76251f1a0ae27e
SimHongSub <ghdtjq2038@gmail.com>
2020-12-05 02:27:58 +0900

    [main] add paren check method

--- src/main.cpp
+++ src/main.cpp
@@ -18,22 +18,25 @@ int main(){
     while(1){
         try{
             vector< pair<int, string> > token = lexer.get_Token();
-            result = syntax.analyze(token, &variables);
+
+            if(syntax.is_valid_paren(token)){
+                result = syntax.analyze(token, &variables);
 
             
-            for(int i=0;i<variables.size();i++){
-                cout << variables[i].first << " : ";
+                for(int i=0;i<variables.size();i++){
+                    cout << variables[i].first << " : ";
 
-                variables[i].second.traverse(variables[i].second.getHead());
+                    variables[i].second.traverse(variables[i].second.getHead());
 
+                    cout << endl;
+                }
+                
+                cout << "ret : ";
+                result.traverse(result.getHead());
                 cout << endl;
+            }else{
+                throw Exception(15);
             }
-            
-            cout << "ret : ";
-            result.traverse(result.getHead());
-            cout << endl;
-
-
         }catch(Exception e){
             e.print();
         }
